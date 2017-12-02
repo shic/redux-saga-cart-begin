32a9676ff598e41ed0d2212098ebd94eb3998d7a
uFirstUser <ufirstuser@LV.amendola46>
2017-12-02 18:36:27 +0100

    put the info (dispatch info)

--- src/sagas/currentUserSaga.js
+++ src/sagas/currentUserSaga.js
@@ -8,10 +8,15 @@ import {
 //Generator funcion
 //Get current user (get it once only)
 export function* currentUserSaga () {
+    //get current use id
     const { id }  = yield take(GET_CURRENT_USER_INFO);
     const response = yield call(fetch,`http://localhost:8081/user/${id}`);
     console.info('response', response);
 
+    //get current use info
     const data = yield apply(response, response.json);
     console.info('data', data);
+
+    //Invoke the setCurrentUser action
+    yield put(setCurrentUser(data));
 }
